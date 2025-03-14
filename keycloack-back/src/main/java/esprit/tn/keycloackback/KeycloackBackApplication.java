6be94ee476daf38f454f8508a1408bec7827f77b
codezella-hub <benslimenlouay29@gmail.com>
2025-03-14 17:45:03 +0100

    new project

---
+++ keycloack-back/src/main/java/esprit/tn/keycloackback/KeycloackBackApplication.java
@@ -0,0 +1,13 @@
+package esprit.tn.keycloackback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloackBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloackBackApplication.class, args);
+    }
+
+}
---
+++ keycloack-back/src/test/java/esprit/tn/keycloackback/KeycloackBackApplicationTests.java
@@ -0,0 +1,13 @@
+package esprit.tn.keycloackback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloackBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
